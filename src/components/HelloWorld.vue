acbfa45b000c3327acf78b42e77d24061de3ef4b
林优斌 <linyoubin@ffcs.cn>
2023-04-15 17:29:13 +0800

    【初始化】新建项目

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,65 +1,65 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript" target="_blank" rel="noopener">typescript</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ pageObj.msg }}</h1>
+    <!-- <img src="@/assets/img/iphone.png" alt=""> -->
+    <!-- <img :src="iphoneImg" alt=""> -->
+    <!-- <img class="phone" src="" alt=""> -->
+    <div class="m-box">
+      <videoPlayer
+        ref="videoPlayerRef"
+        :options="playerOptions"
+        @pause="onPlayerPause($event)"
+      />
+    </div>
+    <!-- <div @click="showVideo" class="btn">播放</div> -->
   </div>
 </template>
 
-<script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-
-@Options({
-  props: {
-    msg: String
+<script setup lang="ts">
+import { defineProps, reactive, ref } from 'vue';
+// 引入媒体
+import videoPlayer from "vue-video-player";   // 如果使用全局引入 这句话可以不写，
+import "videojs-flash";  // 如果使用全局引入 这句话可以不写，
+  const props = defineProps({
+    msg: {
+      type: String
+    }
+  })
+  const pageObj = reactive ({
+    msg: props.msg,
+    // img: require('https://www.apple.com.cn/v/iphone-14-pro/f/images/key-features/features/sizes/size_promax_deep_purple__ct7arcvi3vyq_large.jpg')
+  })
+  const videoPlayerRef = ref(null);
+  const playerOptions = reactive ({
+    height: "300",
+    width: "600",
+    sources: [
+      {
+        type: "rtmp/mp4",
+        src: "rtmp://58.200.131.2:1935/livetv/hunantv",
+      },
+    ],
+    techOrder: ["flash"],
+    autoplay: true,
+    controls: true,
+  })
+  var onPlayerPause = (player: string) => {
+    console.log(player);
   }
-})
-export default class HelloWorld extends Vue {
-  msg!: string
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.hello {
+  width: 100%;
+  .phone {
+    width: 30%;
+    height: auto;
+  }
+  .m-box {
+    width: 600px;
+    height: 420px;
+    background-color: rgba(0, 0, 0, .7);
+  }
 }
 </style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+// import path from 'path'
+// const resolve = (dir: string) => path.resolve(__dirname, dir)
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 别名设置
+  // resolve: {
+  //   alias: {
+  //     '@': resolve('./src'), // 把 @ 指向到 src 目录去
+  //   },
+  // },
 })
